628214c3f3826e80860676fe1748d9814d1f1654
arthurabreu00 <arthurabreu00@gmail.com>
2018-11-14 16:44:21 -0200

    Parte do audio

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -5,10 +5,15 @@ export class MicrophoneController extends ClassEvent{
 
         super();
 
+        this._mimeType = "audio/webm";
+
+        this._availabel = false;
+
+        //  Parte que faz a captação da audio. (Ps: Captação != Gravação)
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(stream=>{
-
+            this._availabel = true;
             this._stream = stream;
 
             let audio = new Audio();
@@ -16,7 +21,7 @@ export class MicrophoneController extends ClassEvent{
             audio.src = URL.createObjectURL(stream); // Criação da URL para ser executado pelo play. 
             //Atráves de uma tag <audio> a será direcionando com o atributo src.
 
-            audio.play(); // O método play funciona executando uma URL. Este método toca de fato o audio.
+            audio.play(); // O método play funciona executando uma URL. Este método toca de fato o audio. 
 
             this.trigger('play',audio, this._stream,);
 
@@ -28,11 +33,74 @@ export class MicrophoneController extends ClassEvent{
 
     stop(){
 
-        // Este método interrompe a gravação do audio.
-        
+        // Este método interrompe a REPRODUÇÃO do audio.
+        this._availabel = false;
+
         this._stream.getTracks().forEach(track =>{
             track.stop();  
         });
 
     } // Fechando o método stop();
+
+
+    isAvailabel(){
+        return this._availabel;
+    }
+
+    startRecord(){
+        // Esté método inicia GRAVAÇÃO da audio. (Ps: Captação != Gravação).
+
+        if(this.isAvailabel()){
+
+            this._mediaRecorder = new MediaRecorder(this._stream,this._mimeType);
+
+            // A classe me retorna pedaços gravados ('chuncks').
+            this._recordChuncks = [];
+
+            this._mediaRecorder.addEventListener('dataavalilabel', e=>{
+
+                // Se tiver algo gravado, junto aos elementos em um array (vetor).
+                if(e.data.size > 0) this._recordChuncks.push(e.data)
+
+            });
+
+            this._mediaRecorder.addEventListener('stop',e =>{
+
+                // Ao parar o audio é necessário, gerar um arquivo de audio.
+
+                // Para isso é necessário a manipulação de binarios. Para isso utilizaremos a classe Blob.
+                
+                let blob = new Blob(this._recordChuncks,{
+                    type: this._mimeType
+                })   // Transformando os arquivos em binarios. e passando meta-dados, para ser transformados em um arquivo.
+             
+                let filename = `rec${Date.now()}.webm` 
+                // Definindo o nome do arquivo, conforme o momento de geração do arquivo e adiconando a extensão do msm.
+
+                let file = new File([blob],filename,{
+
+                    type:this._mimeType,
+                    lastModified: Data.now()
+
+                }); // File precisa de array de binarios, O nome do arquivo e os metaDados do mesmo.
+                    // A classe file, transforma binarios em arquivos e a união dos demais, dados.
+
+                console.log(file);  
+
+            });
+        }
+
+    }
+
+    stopRecord(){
+        // Esté método termina a GRAVAÇÃO da audio. (Ps: Captação != Gravação).
+
+        if(this.isAvailabel()){
+
+            this._mediaRecorder.stop(); // Parando de gravar.
+            this.stop(); // Parando de reproduizir.
+        }
+
+    }
+
 }
